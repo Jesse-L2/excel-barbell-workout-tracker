188c32cd34d8763a6c955bbc2547e46959fd201b
Jesse <jesse.little@utexas.edu>
2022-04-28 12:06:35 -0500

    First commit. Basic functionality of weight_calc and updating spreadsheet added.

---
+++ main.py
@@ -0,0 +1,101 @@
+from openpyxl import Workbook, load_workbook
+import weight_calculator
+
+
+def main():
+    try:
+        wb = load_workbook('Workout.xlsx', data_only=True)
+    except FileNotFoundError:
+        wb = Workbook()
+        wb.save(filename="Workout.xlsx")
+
+    maxes = wb['Maxes']
+    upper_1 = wb['Upper1']
+    lower_1 = wb['Lower1']
+    upper_2 = wb['Upper2']
+    lower_2 = wb['Lower2']
+    theo_maxes = wb['Theoretical Weight Scheme']
+
+    '''Sample code for updating one single cell
+    weight = upper_1['D4'].value # float
+    final_weights = weight_calculator.weight_calc(weight)
+    final_weights = str(final_weights)
+    upper_1['F4'] = final_weights'''
+
+    # J Bench
+    # Upper1-BP
+    for i in range(3, 10):
+        bench_total = upper_1.cell(row=i, column=4).value
+        upper_1.cell(row=i, column=6).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+        # J OHP -D1
+    for i in range(3, 9):
+        ohp_total = upper_1.cell(row=i, column=11).value
+        upper_1.cell(row=i, column=13).value = str(weight_calculator.weight_calc(ohp_total)).replace('[', '').replace(
+            ']', '')
+    # Upper2-BP
+    for i in range(3, 8):
+        bench_total = upper_2.cell(row=i, column=4).value
+        upper_2.cell(row=i, column=6).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+    # Upper2-CG Bench
+    for i in range(11, 15):
+        bench_total = upper_2.cell(row=i, column=4).value
+        upper_2.cell(row=i, column=6).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+
+    # T Bench
+    # Upper1-Bench
+    for i in range(3, 10):
+        bench_total = upper_1.cell(row=i, column=5).value
+        upper_1.cell(row=i, column=7).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+    # Upper2-Bench
+    for i in range(3, 8):
+        bench_total = upper_2.cell(row=i, column=5).value
+        upper_2.cell(row=i, column=7).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+    # Upper2-CG Bench
+    for i in range(11, 15):
+        bench_total = upper_2.cell(row=i, column=5).value
+        upper_2.cell(row=i, column=7).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
+            ']', '')
+
+    # Lower Days
+    # J Lower1-S
+    for i in range(3, 10):
+        squat_total = lower_1.cell(row=i, column=4).value
+        lower_1.cell(row=i, column=6).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
+            ']', '')
+    # J Lower1-SDL
+    for i in range(13, 17):
+        sdl_total = lower_1.cell(row=i, column=4).value
+        lower_1.cell(row=i, column=6).value = str(weight_calculator.weight_calc(sdl_total)).replace('[', '').replace(
+            ']', '')
+    # J Lower2-CDL
+    for i in range(3, 8):
+        cdl_total = lower_2.cell(row=i, column=4).value
+        lower_2.cell(row=i, column=5).value = str(weight_calculator.weight_calc(cdl_total)).replace('[', '').replace(
+            ']', '')
+    # J Lower2-FS
+    for i in range(12, 18):
+        squat_total = lower_2.cell(row=i, column=4).value
+        lower_2.cell(row=i, column=6).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
+            ']', '')
+
+    # T Lower1-S
+    for i in range(3, 10):
+        squat_total = lower_1.cell(row=i, column=5).value
+        lower_1.cell(row=i, column=7).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
+            ']', '')
+    # T Lower2-S
+    for i in range(12, 18):
+        squat_total = lower_2.cell(row=i, column=5).value
+        lower_2.cell(row=i, column=7).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
+            ']', '')
+
+    wb.save('Workout.xlsx')
+
+
+if __name__ == "__main__":
+    main()
---
+++ weight_calculator.py
@@ -0,0 +1,22 @@
+def weight_calc(total):
+    BAR = 45
+    # Note: Input weight * quantity loaded onto bar at a time in pairs
+    # If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s
+    weights = [BAR, 45*2, 35*2, 25*2, 10*2, 10*2, 5*2, 2.5*2, 1*2, 0.75*2, 0.5*2, 0.25*2]
+    weights_used = []
+    # Function runs as long as the total is at least 0.5lbs
+    while total >= 0.5:
+        # If the total is greater than the first (heaviest weight) in the list of weights, subtract off the
+        # weight of the first weight in the list and append it to weights_used, then remove it from the weights list
+        if total >= weights[0]:
+            total -= weights[0]
+            weights_used.append(weights[0])
+
+        del weights[0]
+
+    del weights_used[0]  # removes the bar from the printout
+
+    # Half the total for a set of 2 weight plates
+    return [weights_used/2 for weights_used in weights_used]
+
+
