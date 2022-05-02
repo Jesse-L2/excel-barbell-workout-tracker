865459b68042353a8630a269f7a1a16347efae6f
Jesse-L2 <jesse.little@utexas.edu>
2022-05-02 08:30:09 -0500

    Added default argument to total for weight_calc()

--- weight_calculator.py
+++ weight_calculator.py
@@ -1,13 +1,14 @@
 # Calculates the number of plates to add to the bar to reach a certain weight.
 # Functions with any weight quantity, including micro-plates.
 # weight_calc(int or float)
-def weight_calc(total):
+def weight_calc(total=0):
     BAR = 45
     # Note: Input weight * quantity loaded onto bar at a time in pairs
     # If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s
     weights = [BAR, 45*2, 35*2, 25*2, 10*2, 10*2, 5*2, 2.5*2, 1*2, 0.75*2, 0.5*2, 0.25*2]
     weights_used = []
     # Function runs as long as the total is at least 0.5lbs
+    # TODO: change the while loop total amount to dynamically size to user's smallest weight
     while total >= 0.5:
         # If the total is greater than the first (the heaviest weight) in the list of weights, subtract off the
         # weight of the first weight in the list and append it to weights_used, then remove it from the weights list
