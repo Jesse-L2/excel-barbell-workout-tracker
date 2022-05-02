7afc7cad7299706cc8ef369e449ac8735f4b807b
Jesse-L2 <jesse.little@utexas.edu>
2022-05-02 15:08:42 -0500

    Added more readable line spacing in weight_calculator.py

--- weight_calculator.py
+++ weight_calculator.py
@@ -5,7 +5,7 @@ def weight_calc(total=0):
     BAR = 45
     # Note: Input weight * quantity loaded onto bar at a time in pairs
     # If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s
-    weights = [BAR, 45*2, 35*2, 25*2, 10*2, 10*2, 5*2, 2.5*2, 1*2, 0.75*2, 0.5*2, 0.25*2]
+    weights = [BAR, 45 * 2, 35 * 2, 25 * 2, 10 * 2, 10 * 2, 5 * 2, 2.5 * 2, 1 * 2, 0.75 * 2, 0.5 * 2, 0.25 * 2]
     weights_used = []
     # Function runs as long as the total is at least 0.5lbs
     # TODO: change the while loop total amount to dynamically size to user's smallest weight
@@ -21,7 +21,8 @@ def weight_calc(total=0):
     del weights_used[0]  # removes the bar from the printout
 
     # Half the total for a set of 2 weight plates
-    return [weights_used/2 for weights_used in weights_used]
+    return [weights_used / 2 for weights_used in weights_used]
+
 
 '''
 Notes for future improvements
