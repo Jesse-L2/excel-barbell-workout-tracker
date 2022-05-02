d4e321ade52ba41950e870149fe601a71710f574
Jesse-L2 <jesse.little@utexas.edu>
2022-05-01 19:52:03 -0500

    Bug fix: rather than statically iterating until the weight remaining is less than 0.5 (correct for me, but possibly not for other users), made it so that it is dynamically set to the smallest size in the users list of weights.

--- weight_calculator.py
+++ weight_calculator.py
@@ -7,8 +7,10 @@ def weight_calc(total):
     # If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s
     weights = [BAR, 45*2, 35*2, 25*2, 10*2, 10*2, 5*2, 2.5*2, 1*2, 0.75*2, 0.5*2, 0.25*2]
     weights_used = []
+
     # Function runs as long as the total is at least 0.5lbs
-    while total >= 0.5:
+
+    while total >= weights[-1]:
         # If the total is greater than the first (the heaviest weight) in the list of weights, subtract off the
         # weight of the first weight in the list and append it to weights_used, then remove it from the weights list
         if total >= weights[0]:
