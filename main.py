a7538911fa742d63e5516f202f8b224a05911ee3
Jesse-L2 <jesse.little@utexas.edu>
2022-05-03 10:31:47 -0500

    Changed single comments that spanned multiple line into single multi-line comments and enhanced readability

--- main.py
+++ main.py
@@ -16,54 +16,52 @@ def main():
     lower_2 = wb['Lower2']
     theo_maxes = wb['Theoretical Weight Scheme']
 
-    '''Sample code for updating one single cell
+    """Sample code for updating one single cell
     weight = upper_1['D4'].value # float
     final_weights = weight_calculator.weight_calc(weight)
     final_weights = str(final_weights)
-    upper_1['F4'] = final_weights'''
+    upper_1['F4'] = final_weights"""
 
-    # Bench
-    # Upper1-BP
+    # Upper1 - Bench Press
     # Updating the excel cells, section by section
-    # Note: this code is not as efficient as it could be
     # TODO: Update with function to update each row
     for i in range(3, 10):
         bench_total = upper_1.cell(row=i, column=4).value
         upper_1.cell(row=i, column=5).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
             ']', '')
-    # OHP -D1
+    # Upper1 - Overhead Press
     for i in range(3, 9):
         ohp_total = upper_1.cell(row=i, column=10).value
         upper_1.cell(row=i, column=11).value = str(weight_calculator.weight_calc(ohp_total)).replace('[', '').replace(
             ']', '')
-    #  Upper2-BP
+    #  Upper2 - Bench Press
     for i in range(3, 8):
         bench_total = upper_2.cell(row=i, column=4).value
         upper_2.cell(row=i, column=5).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
             ']', '')
-    #  Upper2-CG Bench
+    #  Upper2 - Close-grip Bench Press
     for i in range(11, 15):
         bench_total = upper_2.cell(row=i, column=4).value
         upper_2.cell(row=i, column=5).value = str(weight_calculator.weight_calc(bench_total)).replace('[', '').replace(
             ']', '')
 
     # Lower Days
-    #   Lower1-S
+    #   Lower1 - Conventional Squat
     for i in range(3, 10):
         squat_total = lower_1.cell(row=i, column=4).value
         lower_1.cell(row=i, column=5).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
             ']', '')
-    #  Lower1-SDL
+    #  Lower1 - Sumo Deadlift
     for i in range(13, 17):
         sdl_total = lower_1.cell(row=i, column=4).value
         lower_1.cell(row=i, column=5).value = str(weight_calculator.weight_calc(sdl_total)).replace('[', '').replace(
             ']', '')
-    #  Lower2-CDL
+    #  Lower2 - Conventional Deadlift
     for i in range(3, 8):
         cdl_total = lower_2.cell(row=i, column=4).value
         lower_2.cell(row=i, column=5).value = str(weight_calculator.weight_calc(cdl_total)).replace('[', '').replace(
             ']', '')
-    #  Lower2-FS
+    #  Lower2 - Front Squat
     for i in range(12, 18):
         squat_total = lower_2.cell(row=i, column=4).value
         lower_2.cell(row=i, column=5).value = str(weight_calculator.weight_calc(squat_total)).replace('[', '').replace(
--- weight_calculator.py
+++ weight_calculator.py
@@ -1,17 +1,17 @@
-# Calculates the number of plates to add to the bar to reach a certain weight.
-# Functions with any weight quantity, including micro-plates.
-# weight_calc(int or float)
+"""Calculates the number of plates to add to the bar to reach a certain weight.
+Functions with any weight quantity, including micro-plates.
+weight_calc(int or float)"""
 def weight_calc(total=0):
     BAR = 45
-    # Note: Input weight * quantity loaded onto bar at a time in pairs
-    # If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s
+    """Note: Input weight * quantity loaded onto bar at a time in pairs
+    If 2 sets of a weight, add to list twice, ex: 10*2, 10*2 for 4 total 10s"""
     weights = [BAR, 45 * 2, 35 * 2, 25 * 2, 10 * 2, 10 * 2, 5 * 2, 2.5 * 2, 1 * 2, 0.75 * 2, 0.5 * 2, 0.25 * 2]
     weights_used = []
     # Function runs as long as the total is at least 0.5lbs
     # TODO: change the while loop total amount to dynamically size to user's smallest weight
     while total >= 0.5:
-        # If the total is greater than the first (the heaviest weight) in the list of weights, subtract off the
-        # weight of the first weight in the list and append it to weights_used, then remove it from the weights list
+        """If the total is greater than the first (the heaviest weight) in the list of weights, subtract off the
+        weight of the first weight in the list and append it to weights_used, then remove it from the weights list"""
         if total >= weights[0]:
             total -= weights[0]
             weights_used.append(weights[0])
@@ -24,10 +24,10 @@ def weight_calc(total=0):
     return [weights_used / 2 for weights_used in weights_used]
 
 
-'''
-Notes for future improvements
+
+"""Notes for future improvements
 1) Hold the weights list in the excel spreadsheet and pull those values into a dict with key = weight,
 and value = quantity (in pairs)
 2) Change the weight_calc function so that rather than deleting an element, it simply starts at the next index
 Reasoning: it is not the best practice to delete items from a list as it is being iterated over
-'''
+"""
